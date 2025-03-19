1d8d577c0296efa20b48fdbaa5c3091c8d0f95f0
otaviofaria5 <otavio.faria@elumini-it.com>
2025-03-19 09:20:27 -0300

    Parte 3

--- src/app/services/api-client/schedules/schedules.service.ts
+++ src/app/services/api-client/schedules/schedules.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { IScheduleService } from './ischedules.service';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { SaveScheduleRequest, SaveScheduleResponse, ScheduleAppointmentMonthResponse } from "./schedule.models";
 
 @Injectable({
   providedIn: 'root'
 })
-export class SchedulesService {
+export class SchedulesService implements IScheduleService {
 
-  constructor() { }
+  private readonly basePath = environment.apiUrl
+
+  constructor(private http: HttpClient) { }
+
+  save(request: SaveScheduleRequest): Observable<SaveScheduleResponse> {
+    return this.http.post<SaveScheduleResponse>(`${this.basePath}schedules`, request)
+  }
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.basePath}schedules/${id}`)
+  }
+  listInMonth(year: number, month: number): Observable<ScheduleAppointmentMonthResponse> {
+    return this.http.get<ScheduleAppointmentMonthResponse>(`${this.basePath}schedules/${year}/${month}`)
+  }
+
 }
