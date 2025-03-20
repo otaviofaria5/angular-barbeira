d8ff21819abe464ba85557ef2885256dc838e2f6
otaviofaria5 <otavio.faria@elumini-it.com>
2025-03-20 09:01:51 -0300

    Finalizado

--- src/app/services/service.token.ts
+++ src/app/services/service.token.ts
@@ -2,11 +2,12 @@ import { InjectionToken } from "@angular/core";
 import { ISnackbarManagerService } from "../services/isnackbar-manager.service";
 import { ICLientService } from "./api-client/clients/iclients.service";
 import { IDialogManagerService } from "./idialog-manager.service";
+import { IScheduleService } from "./api-client/schedules/ischedules.service";
 
 export const SERVICES_TOKEN = {
     HTTP: {
         CLIENT: new InjectionToken<ICLientService>('SERVICES_TOKEN.HTTP.CLIENT'),
-      //  SCHEDULE: new InjectionToken<IScheduleService>('SERVICES_TOKEN.HTTP.SCHEDULE'),
+        SCHEDULE: new InjectionToken<IScheduleService>('SERVICES_TOKEN.HTTP.SCHEDULE'),
     },
     SNACKBAR: new InjectionToken<ISnackbarManagerService>('SERVICES_TOKEN.SNACKBAR'),
     DIALOG: new InjectionToken<IDialogManagerService>('SERVICES_TOKEN.DIALOG')
